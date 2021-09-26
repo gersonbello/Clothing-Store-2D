126aac66c9f724fa2e28dc50fe16b0e0a2c60ec6
Gerson Bello de Goes Filho <gersonbellodegoes@gmail.com>
2021-09-26 03:05:52 -0300

    Addeed some imported sprites in visuals, fixed walk smoke particle effect

---
+++ LSW-Interview-Project/Assets/Scripts/AnimationEventsHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventsHandler : MonoBehaviour
+{
+    [Tooltip("Particle to emit")]
+    public ParticleSystem particleToEmit;
+
+    public void EmitParticle(int amount)
+    {
+        particleToEmit.Emit(amount);
+    }
+}
